1e709ed4d24d449cd215ce0fb38f06b46d26d8cc
DanielM <daniel.martensson100@outlook.com>
2026-01-22 21:07:08 +0100

    Laddar upp buildroot och flyttar firmware -> cubemx

---
+++ watermelon-wine-os/cubemx/CM33/Debug/Common/System/system_stm32mp2xx_m33.d
@@ -0,0 +1,44 @@
+Common/System/system_stm32mp2xx_m33.o: \
+ C:/Users/danie/Documents/GitHub/Watermelon-Wine-Zero-1A/firmware/Common/System/system_stm32mp2xx_m33.c \
+ ../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp2xx.h \
+ ../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp257fxx_cm33.h \
+ ../../Drivers/CMSIS/Include/core_cm33.h \
+ ../../Drivers/CMSIS/Include/cmsis_version.h \
+ ../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../../Drivers/CMSIS/Include/mpu_armv8.h \
+ ../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/system_stm32mp2xx.h \
+ ../Core/Inc/stm32mp2xx_hal_conf.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_def.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_ll_rcc.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc_ex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma_ex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_cortex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_exti.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio_ex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr_ex.h
+../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp2xx.h:
+../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp257fxx_cm33.h:
+../../Drivers/CMSIS/Include/core_cm33.h:
+../../Drivers/CMSIS/Include/cmsis_version.h:
+../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../Drivers/CMSIS/Include/cmsis_gcc.h:
+../../Drivers/CMSIS/Include/mpu_armv8.h:
+../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/system_stm32mp2xx.h:
+../Core/Inc/stm32mp2xx_hal_conf.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_def.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_ll_rcc.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc_ex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma_ex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_cortex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_exti.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio_ex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr_ex.h:
---
+++ watermelon-wine-os/cubemx/CM33/Debug/Drivers/STM32MP2xx_HAL_Driver/stm32mp2xx_hal_cortex.d
@@ -0,0 +1,46 @@
+Drivers/STM32MP2xx_HAL_Driver/stm32mp2xx_hal_cortex.o: \
+ C:/Users/danie/Documents/GitHub/Watermelon-Wine-Zero-1A/firmware/Drivers/STM32MP2xx_HAL_Driver/Src/stm32mp2xx_hal_cortex.c \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal.h \
+ ../Core/Inc/stm32mp2xx_hal_conf.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_def.h \
+ ../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp2xx.h \
+ ../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp257fxx_cm33.h \
+ ../../Drivers/CMSIS/Include/core_cm33.h \
+ ../../Drivers/CMSIS/Include/cmsis_version.h \
+ ../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../../Drivers/CMSIS/Include/mpu_armv8.h \
+ ../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/system_stm32mp2xx.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_ll_rcc.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc_ex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma_ex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_cortex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_exti.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio_ex.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr.h \
+ ../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr_ex.h
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal.h:
+../Core/Inc/stm32mp2xx_hal_conf.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_def.h:
+../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp2xx.h:
+../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/stm32mp257fxx_cm33.h:
+../../Drivers/CMSIS/Include/core_cm33.h:
+../../Drivers/CMSIS/Include/cmsis_version.h:
+../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../Drivers/CMSIS/Include/cmsis_gcc.h:
+../../Drivers/CMSIS/Include/mpu_armv8.h:
+../../Drivers/CMSIS/Device/ST/STM32MP2xx/Include/system_stm32mp2xx.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_ll_rcc.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_rcc_ex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_dma_ex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_cortex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_exti.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_gpio_ex.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr.h:
+../../Drivers/STM32MP2xx_HAL_Driver/Inc/stm32mp2xx_hal_pwr_ex.h:
