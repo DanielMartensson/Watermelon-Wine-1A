82f9153e838c4ffc347378779c91582cabca4c77
DanielM <daniel.martensson100@outlook.com>
2026-01-22 21:16:59 +0100

    Gör buildroot till icke submoduler

--- firmware/CA35/DeviceTree/firmware/optee-os/conf.mk
+++
@@ -1,4 +0,0 @@
-# SPDX-License-Identifier: BSD-2-Clause
-
-flavor_dts_file-MP25 = stm32mp257f-firmware-mx.dts
-flavorlist-MP25 += $(flavor_dts_file-MP25)
--- firmware/CM33/Debug/Core/Startup/startup_stm32mp257fakx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32mp257fakx.o: \
- ../Core/Startup/startup_stm32mp257fakx.s
